62f4689d011d68178d625dc8e31e44a3258bbdde
Ricardo Hernández <ricardohs35@gmail.com>
2025-08-25 15:12:50 -0600

    Tweak floaterm configuration

--- nvim-personal/lua/user/plugins/floaterm.lua
+++ nvim-personal/lua/user/plugins/floaterm.lua
@@ -6,10 +6,13 @@ return {
     { '<C-t>', ':FloatermToggle<CR>' },
     { '<C-t>', '<Esc>:FloatermToggle<CR>', mode = 'i' },
     { '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', mode = 't' },
+    { '<C-g>', ':FloatermNew lazygit<CR>' },
+    { '<C-g>', '<C-\\><C-n>:FloatermToggle<CR>', mode = 't' },
   },
-  cmd = { 'FloatermToggle' },
+  cmd = { 'FloatermToggle', 'FloatermNew' },
   init = function()
-    vim.g.floaterm_width = 0.8
-    vim.g.floaterm_height = 0.8
+    -- vim.g.floaterm_width = 0.8
+    vim.g.floaterm_height = 0.6
+    vim.g.floaterm_wintype = 'split'
   end,
 }
