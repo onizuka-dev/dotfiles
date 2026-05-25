140aa1796707946f62766e46cbeee7c5cd488a2b
Ricardo Hernández <ricardohs35@gmail.com>
2026-05-25 17:23:57 -0600

    ✨ Add Claude floaterm binding and center the window
    Bind <A-c> to open Claude in a new floaterm (with toggle behavior in
    terminal mode), and switch the floaterm layout to a centered 95%×95%
    window instead of a bottom split.

--- nvim-personal/lua/user/plugins/floaterm.lua
+++ nvim-personal/lua/user/plugins/floaterm.lua
@@ -8,11 +8,16 @@ return {
     { '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', mode = 't' },
     { '<C-g>', ':FloatermNew lazygit<CR>' },
     { '<C-g>', '<C-\\><C-n>:FloatermToggle<CR>', mode = 't' },
+    { '<A-c>', ':FloatermNew claude<CR>' },
+    { '<A-c>', '<C-\\><C-n>:FloatermToggle<CR>', mode = 't' },
   },
   cmd = { 'FloatermToggle', 'FloatermNew' },
   init = function()
-    -- vim.g.floaterm_width = 0.8
-    vim.g.floaterm_height = 0.6
-    vim.g.floaterm_wintype = 'split'
+    vim.g.floaterm_width = 0.95
+    vim.g.floaterm_height = 0.95
+    vim.g.floaterm_position = 'center'
+    -- Terminal at bottom
+    -- vim.g.floaterm_height = 0.6
+    -- vim.g.floaterm_wintype = 'split'
   end,
 }
